ca0ef6187e432db3e41d7134be32acddca86a59f
Tozinoo <goldboy122@gmail.com>
2024-11-22 23:16:39 +0900

    mio clone ownership error

--- src/server/events.rs
+++ src/server/events.rs
@@ -1,7 +1,9 @@
 use mio::net::TcpListener;
 use mio::{Events, Poll, Token};
 use std::collections::HashMap;
-
+use std::sync::Arc;
+use std::thread;
+use rayon::ThreadPoolBuilder;
 use crate::client;
 
 /// Poll에 등록할 기본 Interest 설정
@@ -12,39 +14,75 @@ pub fn listener_interest() -> mio::Interest {
 /// 이벤트 처리
 pub fn handle_events(
     poll: &mut Poll,
-    listener: &mut TcpListener,
+    listener: Arc<TcpListener>,
     clients: &mut HashMap<Token, (mio::net::TcpStream, Vec<u8>)>,
     next_client_token: &mut usize,
     events: &mut Events,
 ) -> std::io::Result<()> {
-    let mut to_remove = Vec::new();
+    let mut to_remove: Vec<Token> = Vec::new();
+    let threadpool = ThreadPoolBuilder::new().num_threads(4).build().unwrap();
 
     for event in events.iter() {
         match event.token() {
             Token(0) => {
                 // Listener 이벤트 처리 (새로운 클라이언트 연결)
                 if event.is_readable() {
-                    if let Ok((stream, addr)) = listener.accept() {
+                    if let Ok((mut stream, addr)) = listener.accept() {
                         println!("Accepted connection from {}", addr);
                         client::register_client(poll, clients, stream, next_client_token)?;
+                        println!("clients {:?}", clients);
                     }
                 }
             }
             token => {
                 // 클라이언트 이벤트 처리
                 if let Some((stream, buffer)) = clients.get_mut(&token) {
-                    if event.is_readable() {
-                        if !client::handle_read(token, stream, buffer)? {
-                            to_remove.push(token);
+                    threadpool.spawn(move || {
+                        let readable = client::handle_read(token, stream, buffer).expect("readable error");
+                        let writable = client::handle_write(token, stream, buffer).expect("readable error");
+
+                        if event.is_readable() {
+                            if readable {
+                                to_remove.push(token);
+                            }
                         }
-                    }
-                    if event.is_writable() {
-                        if !client::handle_write(token, stream, buffer)? {
-                            to_remove.push(token);
+                        if event.is_writable() {
+                            if writable {
+                                to_remove.push(token);
+                            }
                         }
-                    }
+                    });
                 }
-            }
+
+
+                // if let Some((mut stream, buffer)) = clients.remove(&token) {
+                //     let buffer_clone = buffer.clone();
+                //     let token_clone = token;
+                //     let readable = event.is_readable();
+                //     let writable = event.is_writable();
+                //     println!("readable {}", readable);
+                //     println!("writable {}", writable);
+                //     println!("clients {:?}", clients);
+                //
+                //     threadpool.spawn(move || {
+                //         if readable {
+                //             let mut buf = buffer_clone.clone();
+                //             if let Ok(success) = client::handle_read(token_clone, &mut stream, &mut buf) {
+                //                 if !success {
+                //                     println!("Disconnecting client {:?}", token_clone);
+                //                 }
+                //             }
+                //         }
+                //         if writable {
+                //             let mut buf = buffer_clone.clone();
+                //             if let Ok(success) = client::handle_write(token_clone, &mut stream, &mut buf) {
+                //                 if !success {
+                //                     println!("Disconnecting client {:?}", token_clone);
+                //                 }
+                //             }
+                //         }
+                //     });
+                }
         }
     }
 
--- src/server/listener.rs
+++ src/server/listener.rs
@@ -1,10 +1,11 @@
+use std::collections::HashMap;
 use std::thread;
 use mio::net::TcpListener;
-use mio::{Events, Poll, Token};
-use std::collections::HashMap;
+use mio::{Events, Interest, Poll, Token};
 use std::io::{Read, Write};
 use std::net::{IpAddr, SocketAddr};
-
+use std::sync::Arc;
+use rayon::ThreadPoolBuilder;
 use crate::server::events;
 
 const LISTENER: Token = Token(0);
@@ -13,54 +14,29 @@ pub fn run(server_ip: IpAddr, server_port: u16) -> std::io::Result<()> {
     let server_address = SocketAddr::new(server_ip, server_port);
 
     let mut listener = TcpListener::bind(server_address)?;
-    // let mut poll = Poll::new()?;
-    // 
-    // poll.registry()
-    //     .register(&mut listener, LISTENER, events::listener_interest())?;
 
-    // let mut events = Events::with_capacity(128);
-    // 
-    // let mut clients: HashMap<Token, (mio::net::TcpStream, Vec<u8>)> = HashMap::new();
-    // let mut next_client_token: usize = 1;
+    let mut poll = Poll::new()?;
+    poll.registry()
+        .register(&mut listener, LISTENER, Interest::READABLE | Interest::WRITABLE).expect("poll error");
+
+    let mut events = Events::with_capacity(128);
+    let mut clients: HashMap<Token, (mio::net::TcpStream, Vec<u8>)> = HashMap::new();
+    let mut next_client_token: usize = 1;
+
+    let listener = Arc::new(listener);
+
 
     println!("Server listening on {}", server_address);
 
-
     loop {
-        if let Ok((mut stream, addr)) = listener.accept() {
-            println!("Accepted connection from {}", addr);
-            thread::spawn(move || {
-                let mut buffer = [0;1024];
-                loop {
-                    match stream.read(&mut buffer) {
-                        Ok(0) => {
-                            println!("Client at {} disconnected", addr);
-                            break;
-                        }
-                        Ok(n) => {
-                            println!("Received from {}: {}", addr, String::from_utf8_lossy(&buffer[..n]));
-                            if stream.write_all(&buffer[..n]).is_err() {
-                                println!("Failed {} ", addr);
-                                break;
-                            }
-                        }
-                        Err(e) => {
-                            println!("Error: {}", e);
-                            break;
-                        }
-                    }
-                }
-            });
-            
-        }
-        // poll.poll(&mut events, None)?;
-        // 
-        // events::handle_events(
-        //     &mut poll,
-        //     &mut listener,
-        //     &mut clients,
-        //     &mut next_client_token,
-        //     &mut events,
-        // )?;
+        poll.poll(&mut events, None);
+
+        events::handle_events(
+            &mut poll,
+            Arc::clone(&listener),
+            &mut clients,
+            &mut next_client_token,
+            &mut events,
+        )?;
     }
 }
